86fa1e7b6a62d5dacac4aab7c7664e4d2f8ecde9
Joe SHI <qiaoshi@amazon.com>
2020-04-11 22:41:25 +0800

    use new pipeline

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #
-# This assumes all of the OS-level configuration has been completed and git repo has already been cloned
-#
 # This script should be run from the repo's deployment directory
 # cd deployment
-# ./build-s3-dist.sh source-bucket-base-name trademarked-solution-name version-code
+# ./build-s3-dist.sh source-bucket-base-name solution-name version-code template-bucket-name
 #
-# Paramenters:
+# Parameters:
 #  - source-bucket-base-name: Name for the S3 bucket location where the template will source the Lambda
 #    code from. The template will append '-[region_name]' to this bucket name.
 #    For example: ./build-s3-dist.sh solutions my-solution v1.0.0
 #    The template will then expect the source code to be located in the solutions-[region_name] bucket
 #
-#  - trademarked-solution-name: name of the solution for consistency
+#  - solution-name: name of the solution for consistency
 #
 #  - version-code: version of the package
+#
+#  - template-bucket-name: Name for S3 bucket location where the template will be located in.
 
 # Check to see if input has been provided:
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
     echo "Please provide the base source bucket name, trademark approved solution name and version where the lambda code will eventually reside."
-    echo "For example: ./build-s3-dist.sh solutions trademarked-solution-name v1.0.0"
+    echo "For example: ./build-s3-dist.sh solutions trademarked-solution-name v1.0.0 solutions-reference"
     exit 1
 fi
 
